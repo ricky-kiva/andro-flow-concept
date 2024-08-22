77eea2d6c6fc02f942f888280f7e413829d8405c
ricky-kiva <ricky.kiva@gmail.com>
2024-08-22 21:43:31 +0700

    feat(v-model): impl. Flow Terminal Operator

--- app/src/main/java/com/rickyslash/kotlinflowconcept/MainViewModel.kt
+++ app/src/main/java/com/rickyslash/kotlinflowconcept/MainViewModel.kt
@@ -34,6 +34,7 @@ class MainViewModel: ViewModel() {
         viewModelScope.launch {
             collectBasicFlow()
             collectSimpleOperator()
+            collectTerminalOperator()
         }
     }
 
@@ -55,6 +56,29 @@ class MainViewModel: ViewModel() {
             .map { time -> time * time }
             .onEach { time -> Log.d(TAG, "2.1.A Logged on each: $time") }
             .collect { time -> Log.d(TAG, "2.1.B Augmented Flow: $time") }
+    }
+
+    // 2.2 Terminal Flow Operator: Last operator to be operated on Flow collection
+    // - count  -> count number of Flow items after operated
+    // - reduce -> do operation on Flow items & accumulate the result
+    // - fold   -> reduce with initial value
+    private suspend fun collectTerminalOperator() {
+        val countResult = countdownFlow
+            .count { it % 2 == 0 }
+
+        Log.d(TAG, "2.2.1 Count of filtered Flow: $countResult")
+
+        val reduceResult = countdownFlow
+            .filter { it % 2 == 1 }
+            .reduce{ acc, item -> acc + item }
+
+        Log.d(TAG, "2.2.2 Accumulated sum of odd Flow: $reduceResult")
+
+        val foldResult = countdownFlow
+            .filter { it % 2 == 1 }
+            .fold(100) { acc, item -> acc + item }
+
+        Log.d(TAG, "2.2.3 Accumulated sum of odd Flow by initial of 100: $foldResult")
     }
 
     companion object {
